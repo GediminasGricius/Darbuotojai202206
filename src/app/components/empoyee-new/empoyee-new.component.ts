a31982d797098391af59a68aa0b1057f71b571a2
GediminasGricius <g.gricius@ituostas.lt>
2022-06-23 19:31:23 +0300

    init

--- src/app/components/empoyee-new/empoyee-new.component.ts
+++ src/app/components/empoyee-new/empoyee-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-empoyee-new',
@@ -12,27 +12,57 @@ export class EmpoyeeNewComponent implements OnInit {
 
   constructor() { 
     this.eForm=new FormGroup({
-      'name': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
+      'name': new FormControl(null, [this.uzdraustiVardai, Validators.required]),
       'surname':new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(16)]),
       'gender':new FormControl('male'),
-      'email':new FormControl(null, [Validators.email, Validators.required])
+      'email':new FormControl(null, [Validators.email, Validators.required]),
+      'phones':new FormArray([]),
+      'addresses':new FormArray([])
     });
   }
 
   ngOnInit(): void {
     
   }
-/*
-  uzdraustiVardai(control:FormControl): {[s:string]:boolean}|null{
+
+  uzdraustiVardai(control:AbstractControl): {[s:string]:boolean}|null{
     if (control.value=="Jonas"){
       return {'vardasNegalimas':true}
     }else{
       return null;
     }
   }
-*/
+
   public newEmployee(){
     console.log(this.eForm);
+  }
+
+  public addPhone(){
+    const phone=new FormControl(null, Validators.required);
+    (<FormArray> this.eForm.get('phones')).push(phone);
+
+    //(this.eForm.get('phones') as FormArray).push
+
+  }
+
+  public phones(){
+    return (<FormArray> this.eForm.get('phones')).controls;
+  }
+
+  public addAddress(){
+    const address=new FormGroup({
+      city:new FormControl(null,Validators.required),
+      street:new FormControl(null, Validators.required)
+    });
+    (<FormArray> this.eForm.get('addresses')).push(address);
+  }
+
+  public addresses(){
+    return (<FormArray> this.eForm.get('addresses')).controls;
+  }
+
+  public abstractToFormGroup(formGroup:AbstractControl){
+    return <FormGroup>formGroup;
   }
 
 }
